56c7e96d7e916aedbbce2a40b7f5988854843635
Ukra <joantx.cs@gmail.com>
2023-10-24 13:28:46 -0300

    port change

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ProductManager = require('./ProductManager'); 
 
 const app = express();
-const port = 3000; 
+const port = 8080; 
 
 const productManager = new ProductManager('productos.json'); 
 
