0337cc60519cdef59dff82520449dfb3fe694f8b
Ukra <joantx.cs@gmail.com>
2024-01-15 09:45:55 -0300

    changes passport

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const User = require('../models/User');
+const User = require('../dao/models/User');
 require('dotenv').config();
 
 module.exports = () => {
