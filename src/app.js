5a56a78bf445f570f850966ba72dc3207a6da512
Ukra <joantx.cs@gmail.com>
2024-02-06 19:37:53 -0300

    fix req.logger

--- src/app.js
+++ src/app.js
@@ -72,13 +72,13 @@ app.use('/api/sessions', sessionRoutes);
 
 // Endpoint para probar los logs
 app.get('/loggerTest', (req, res) => {
+  console.log(req.logger);
   req.logger.debug('Este es un mensaje de debug');
   req.logger.http('Este es un mensaje HTTP');
   req.logger.info('Este es un mensaje de información');
-  req.logger.warning('Este es un mensaje de advertencia');
+  req.logger.warn('Este es un mensaje de advertencia'); // Cambiado de warning a warn
   req.logger.error('Este es un mensaje de error');
-  req.logger.fatal('Este es un mensaje fatal');
-
+  req.logger.silly('Este es un mensaje fatal'); // Cambiado de fatal a silly
   // Mensaje de confirmación
   res.send('Logs generados');
 });
--- src/routes/loggerTest.js
+++ src/routes/loggerTest.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-// Definir el endpoint para probar los logs
-router.get('/loggerTest', (req, res) => {
+// Endpoint para probar los logs
+app.get('/loggerTest', (req, res) => {
+    console.log(req.logger);
     req.logger.debug('Este es un mensaje de debug');
     req.logger.http('Este es un mensaje HTTP');
     req.logger.info('Este es un mensaje de información');
-    req.logger.warning('Este es un mensaje de advertencia');
+    req.logger.warn('Este es un mensaje de advertencia'); // Cambiado de warning a warn
     req.logger.error('Este es un mensaje de error');
-    req.logger.fatal('Este es un mensaje fatal');
-
+    req.logger.silly('Este es un mensaje fatal'); // Cambiado de fatal a silly
     // Mensaje de confirmación
     res.send('Logs generados');
-});
+  });
+
 
 module.exports = router;
