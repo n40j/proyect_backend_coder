60bdcd83531f4ddcd901d07d8c663e3d0e8fc648
n40__j <joantx.cs@gmail.com>
2024-02-17 13:57:22 -0300

    Revert "Update app.js"
    This reverts commit 4fef53b589c9d31d9621c4e17a508ceee29da69a.

--- src/app.js
+++ src/app.js
@@ -13,6 +13,20 @@ const port = 8080;
 const connectDB = require("./db.js");
 connectDB();
 
+// Importar e inicializar Passport
+const initPassport = require("./config/passport");
+initPassport();
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Función de middleware para verificar la autenticación
+const ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+};
+
 // Configuración del logger
 const logger = winston.createLogger({
   level: 'info',
@@ -39,27 +53,11 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.json());
 app.use(express.static('public'));
-
-// Configuración de express-session
 app.use(session({
   secret: 'your-secret-key',
   resave: false,
   saveUninitialized: false
 }));
-
-// Inicialización de Passport
-const initPassport = require("./config/passport");
-initPassport();
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Función de middleware para verificar la autenticación
-const ensureAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/login');
-};
 
 // Ruta para la página de inicio
 app.get('/', (req, res) => {
@@ -72,7 +70,37 @@ app.get('/', (req, res) => {
   }
 });
 
-// Resto de tus rutas y configuraciones aquí...
+// Rutas para el manejo de autenticación
+app.use('/login', require('./routes/login'));
+app.use('/logout', require('./routes/logout'));
+app.use('/register', require('./routes/register'));
+
+// Nueva ruta para /dashboard 
+app.get('/dashboard', ensureAuthenticated, (req, res) => {
+  // Renderiza el panel de control o la página del dashboard
+  res.render('dashboard', { user: req.user }); 
+});
+
+// Importa la ruta para productos de mocking
+const mockingRoute = require('./routes/mockingRoute');
+app.use('/mockingproducts', mockingRoute);
+
+// Importa y utiliza la ruta para las sesiones
+const sessionRoutes = require('./routes/session');
+app.use('/api/sessions', sessionRoutes);
+
+// Endpoint para probar los logs
+app.get('/loggerTest', (req, res) => {
+  console.log(req.logger);
+  req.logger.debug('Este es un mensaje de debug');
+  req.logger.http('Este es un mensaje HTTP');
+  req.logger.info('Este es un mensaje de información');
+  req.logger.warn('Este es un mensaje de advertencia'); // Cambiado de warning a warn
+  req.logger.error('Este es un mensaje de error');
+  req.logger.silly('Este es un mensaje fatal'); // Cambiado de fatal a silly
+  // Mensaje de confirmación
+  res.send('Logs generados');
+});
 
 // Middleware de manejo de errores
 const { errorHandler } = require('./utils/errorHandler'); 
